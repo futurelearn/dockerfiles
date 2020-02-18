d3a14e562191626f781fbd8f10d84e674c873ac6
Laura Martin <surminus@gmail.com>
2020-02-18 17:08:13 +0000

    Add S3 cache script to the image
    This script was used to populate a local cache for all our dependencies
    which drastically speeds up our builds.
    
    Including it into the image means it can be a drop in replacement
    without having to amend our Jenkins files, although really it should be
    a Jenkins library.

---
+++ jenkins-agent-futurelearn/s3-cache.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -eo pipefail
+
+if [ -z "$MOUNT" ]; then
+  echo "Must specify directories to cache. Exiting!"
+  exit 1
+fi
+
+if [ -z "$CACHE_BUCKET" ]; then
+  echo "Must specify CACHE_BUCKET environment variable"
+  exit 1
+fi
+
+VERBOSE=""
+
+if [[ -n "$VERBOSE" && "$VERBOSE" == "true" ]]; then
+  VERBOSE="-v"
+fi
+
+# Convert forward slashes to escaped forward slashes
+SED_SAFE_GIT_BRANCH=$(echo "$GIT_BRANCH" | sed 's/\//\\\//g')
+
+MAIN_JOB_NAME=$(echo "$JOB_NAME" | sed "s/\/${SED_SAFE_GIT_BRANCH}//g")
+CACHE_PATH="${MAIN_JOB_NAME}/${GIT_BRANCH}"
+FALLBACK_PATH="${MAIN_JOB_NAME}/master"
+
+PATHS_TO_TAR=$(echo $MOUNT | sed 's/,/ /g')
+
+echo "Starting at $(date)"
+
+if [[ $1 == "rebuild" ]]; then
+
+  PATHS=""
+  echo "Compressing selected paths:"
+  for i in $PATHS_TO_TAR; do
+    if test -e "$i"; then
+      PATHS+=" ${i}"
+      echo "Adding ${i}"
+    else
+      echo "Cannot find ${i}. Skipping."
+    fi
+  done
+
+  if [[ $PATHS == "" ]]; then
+    echo "No paths found for cache. Moving on..."
+    exit 0
+  else
+    tar cf - "$PATHS" | pigz $VERBOSE > archive.tgz
+  fi
+
+  echo "Compression complete, uploading to S3"
+
+  aws s3 cp --quiet ./archive.tgz "s3://${CACHE_BUCKET}/${CACHE_PATH}/archive.tgz"
+
+  echo "Upload completed!"
+
+  echo "Finished! Exiting at $(date)" && exit 0
+elif [[ $1 == "restore" ]]; then
+
+  if aws s3 cp --quiet "s3://${CACHE_BUCKET}/${CACHE_PATH}/archive.tgz archive.tgz"; then
+    echo "Cache downloaded successfully."
+  elif aws s3 cp --quiet "s3://${CACHE_BUCKET}/${FALLBACK_PATH}/archive.tgz archive.tgz"; then
+    echo "Cache downloaded successfully."
+  else
+    echo "Cannot find cache. Skipping!"
+    exit 0
+  fi
+
+  echo "Uncompressing cache file."
+  unpigz $VERBOSE < archive.tgz | tar $VERBOSE -xC .
+  echo "Cache uncompressed."
+
+  echo "Finished! Exiting at $(date)" && exit 0
+else
+  echo "Must provide either restore or rebuild as first argument. Exiting!"
+  exit 1
+fi
