c7685317a39e2ae1f1d67fcde9472f9c67bcf877
Laura Martin <surminus@gmail.com>
2020-06-05 09:17:16 +0100

    traefik-proxy entrypoint updates
    The `BACKEND_HOST` default value should include the protocol.
    
    I've quoted the environment variables to avoid word splitting.
    
    I've disabled a shellcheck false positive.

--- traefik-proxy/entrypoint.sh
+++ traefik-proxy/entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -e
 
-if [ -z $STATIC_PAGES_HOST ]; then
+if [ -z "${STATIC_PAGES_HOST}" ]; then
   echo "Must set STATIC_PAGES_HOST environment variable"
   exit 1
 fi
 
-[ -z $PORT ] && export PORT=80
-[ -z $BACKEND_HOST ] && export BACKEND_HOST=127.0.0.1
+[ -z "${PORT}" ] && export PORT="80"
+[ -z "${BACKEND_HOST}" ] && export BACKEND_HOST="http://127.0.0.1/"
 
+# shellcheck disable=SC2016
 envsubst '${BACKEND_HOST} ${STATIC_PAGES_HOST} ${PORT}' < /etc/traefik/traefik.yaml.template > /etc/traefik/traefik.yaml
 
 exec "$@"
