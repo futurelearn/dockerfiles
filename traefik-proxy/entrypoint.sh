4d4e20715eddb18f7811e4d7f813859d65129dc3
Laura Martin <surminus@gmail.com>
2020-06-05 14:01:20 +0100

    Traefik log settings

--- traefik-proxy/entrypoint.sh
+++ traefik-proxy/entrypoint.sh
@@ -8,8 +8,9 @@ fi
 
 [ -z "${PORT}" ] && export PORT="80"
 [ -z "${BACKEND_HOST}" ] && export BACKEND_HOST="http://127.0.0.1/"
+[ -z "${LOG_LEVEL}" ] && export LOG_LEVEL="ERROR"
 
 # shellcheck disable=SC2016
-envsubst '${BACKEND_HOST} ${STATIC_PAGES_HOST} ${PORT}' < /etc/traefik/traefik.yaml.template > /etc/traefik/traefik.yaml
+envsubst '${BACKEND_HOST} ${STATIC_PAGES_HOST} ${PORT} ${LOG_LEVEL}' < /etc/traefik/traefik.yaml.template > /etc/traefik/traefik.yaml
 
 exec "$@"
