82b588468b07691a2ead84fcf43e29ab4ecb85b9
Laura Martin <surminus@gmail.com>
2020-06-23 11:59:44 +0100

    Set real IP configuration
    We want to ensure the users remote IP is forwarded rather than an IP
    from the load balancer or Cloudflare.
    
    This users the set_real_ip_from config option[1] to ensure this happens.
    
    We populate the configuration with Cloudflare addresses, and the private
    network range used by our load balancers internally.
    
    [1] https://nginx.org/en/docs/http/ngx_http_realip_module.html

--- nginx-proxy/entrypoint.sh
+++ nginx-proxy/entrypoint.sh
@@ -12,4 +12,19 @@ fi
 # shellcheck disable=SC2016
 envsubst '${BACKEND_HOST} ${STATIC_PAGES_HOST} ${PORT}' < /etc/nginx/default.conf.template > /etc/nginx/conf.d/default.conf
 
+REALIP_CONF_FILE="/etc/nginx/conf.d/http_realip.conf"
+
+echo "set_real_ip_from 10.0.0.0/8;" > $REALIP_CONF_FILE
+
+for i in $(curl -sSL https://www.cloudflare.com/ips-v4); do
+  echo "set_real_ip_from $i;" >> $REALIP_CONF_FILE
+done
+
+for i in $(curl -sSL https://www.cloudflare.com/ips-v6); do
+  echo "set_real_ip_from $i;" >> $REALIP_CONF_FILE
+done
+
+echo "real_ip_header X-Forwarded-For;" >> $REALIP_CONF_FILE
+echo "real_ip_recursive on;" >> $REALIP_CONF_FILE
+
 exec "$@"
